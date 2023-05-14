324d5c93cabe68f24f63212b5197bd886aa8b0d1
Minato <umi0n3@gmail.com>
2023-05-14 21:14:09 +0200

    something bind

---
+++ DbwViewer/Models/Area.cs
@@ -0,0 +1,11 @@
+﻿namespace DbwViewer.Models;
+
+public class Area
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int PrecedentElementId { get; set; }
+    public int LevelId { get; set; }
+    public string? LevelName { get; set; }
+    public bool IsChangeable { get; set; }
+}
--- DbwViewer/ViewModels/MainWindowViewModel.cs
+++ DbwViewer/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace DbwViewer.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Documents;
+using DbwViewer.Models;
+
+namespace DbwViewer.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Name { get; set; } = "Alex";
+    public MainWindowViewModel()
+    {
+        AreaData = new ObservableCollection<Area>
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Ceny",
+                PrecedentElementId = 727,
+                LevelId = 1,
+                LevelName = "Dziedzina",
+                IsChangeable = false
+            },
+            new()
+            {
+                Id = 4,
+                Name = "Budownictwo",
+                PrecedentElementId = 727,
+                LevelId = 1,
+                LevelName = "Dziedzina",
+                IsChangeable = false
+            }
+        };
+    }
+
+    public string AppHeader { get; set; } = "GUS Dziedzinowe Bazy Wiedzy Viewer";
+
+    public ObservableCollection<Area> AreaData { get; set; }
 }
