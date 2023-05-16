09bed7a52b3935dcd2a43b667add4264ab6193de
Minato <umi0n3@gmail.com>
2023-05-16 14:55:37 +0200

    json property

--- DbwViewer/Models/Area.cs
+++ DbwViewer/Models/Area.cs
@@ -1,11 +1,24 @@
-﻿namespace DbwViewer.Models;
+﻿using Newtonsoft.Json;
+
+namespace DbwViewer.Models;
 
 public class Area
 {
+    [JsonProperty("id")]
     public int Id { get; set; }
+    
+    [JsonProperty("nazwa")]
     public string? Name { get; set; }
+    
+    [JsonProperty("id-nadrzedny-element")]
     public int PrecedentElementId { get; set; }
+    
+    [JsonProperty("id-poziom")]
     public int LevelId { get; set; }
+    
+    [JsonProperty("nazwa-poziom")]
     public string? LevelName { get; set; }
+    
+    [JsonProperty("czy-zmienne")]
     public bool IsChangeable { get; set; }
 }
